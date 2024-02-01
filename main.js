b9b4c6948e50315c81b72724965c313fdd5d803b
Ogweno-D <dennisogwene@gmail.com>
2024-02-01 19:04:27 +0300

    Day 1

---
+++ main.js
@@ -0,0 +1,97 @@
+console.log("Hello World");
+
+console.log('Hello World');
+
+// let a=10;
+// const b=20;
+// console.log(a+b);
+
+// let age=20;
+// console.log(age);
+// age=23;
+// console.log(age);
+
+// const name= 'Dennis'
+// const ag =  20
+
+// console.log(name + '' + age )
+
+// //Arrays in Javasript
+// fruits= [ 'oranges', 'bananas', 'mango'];
+// console.log(fruits);
+
+// fruits.push('grapes');
+// fruits[1]='custard'
+// fruits.pop()
+// console.log(Array.isArray(fruits))
+// console.log(Array.toString(fruits))
+// console.log(fruits)
+
+
+// const dog ={
+//     species:'chiwawa',
+//     name:'Bosco',
+//     owners:['Dennis','Edgar' , 'Nijel'],
+//     dog_age: 10,
+//     adress:{
+//         city: 'Kisumu',
+//         street : 'Kaego Strt'
+//     }
+// }
+// console.log(dog.adress.street)
+
+
+// const todo = [
+//     {
+//         id:1,
+//         task: 'Go round the Plot',
+//         time : '2hrs'
+//     },
+
+//     {
+//         id:2,
+//         task: 'Do the dishes',
+//         time : '1hrs'
+//     },
+//     {
+//         id:1,
+//         task: 'Go round the Plot',
+//         time : '2hrs'
+//     }
+// ]
+
+// //Converting string to array
+// console.log(todo[2].id)
+// const todoJSON = JSON.stringify(todo)
+// console.log(todoJSON)
+
+// //For  Loops
+// for (let i = 0; i<10; i++) {
+//      console.log(i)
+//     }
+
+// fruits= [ 'mango','bananas','oranges','guavas','passion fruit']
+// for (let index = 0; index < fruits.length; index++) {
+//     const element = fruits[index];
+//     console.log(element)
+// }
+
+// //While Loops
+
+// let i=0
+// while (i<10) {
+//     i++;
+//  console.log(i)   
+// }
+
+// console.log('+++++++++++++++++++++++++++++++++')
+// console.log("This is a do while loop")
+// console.log("++++++++++++++++++++++++++++++++++")
+// //Do while Loops
+
+// let l=0
+// do {
+//     console.log(l)
+//     l++
+// } while (l<10);
+
